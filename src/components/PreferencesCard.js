ccc6a81a57e8d4194c1090b58ef69083f19106d6
bluebox228 <gritskevitchstefania@gmail.com>
2024-11-22 17:06:55 +0300

    pref cards

---
+++ src/components/PreferencesCard.js
@@ -0,0 +1,12 @@
+import "../styles/preferences.css"
+function PreferencesCard({ imageSrc, title, description }) {
+    return (
+        <div className="preferences-card">
+            <img src={imageSrc} className="preferences-card__image" />
+            <h3 className="preferences-card__title">{title}</h3>
+            <p className="preferences-card__description">{description}</p>
+            <div className="orange-strip"></div>
+        </div>
+    );
+}
+export default PreferencesCard;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import Email from './Email';
 import Location from './components/Location';
 import ContactHeader from './components/ContactHeader'
 import Footer from './components/Footer'
+import PreferencesContainer from './components/PreferencesContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ContactHeader/>
+   <PreferencesContainer/>
     <Location/>
     <Footer/>
   </React.StrictMode>
