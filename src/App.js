adf7a6e6778699c894d8c399aa61f2077fa5b6f3
Vikvillka <bechkovskayavic@gmail.com>
2024-11-22 22:56:37 +0300

    routing without style + folder pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import {Routes, Route, Link} from 'react-router-dom';
 
-import {ContactPage} from './pages/ContactPage';
-import {CatalogPage} from './pages/CatalogPage';
-import {AboutPage} from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import CatalogPage from './pages/CatalogPage';
+import AboutPage from './pages/AboutPage';
 
+import ContactHeader from './components/ContactHeader';
+import Footer from './components/Footer'
+import HomePage from './pages/HomePage';
 
 function App(){
     return(
-        <header>
-            
-        </header>
+      <>
+      <ContactHeader/>
+      <header className='header'>
+        <Link to='/'>Главная</Link>
+        <Link to='/about'>О нас</Link>
+        <Link to='/catalog'>Каталог</Link>
+        <Link to='/contact'>Контакты</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/about" element={<AboutPage/>}/>
+        <Route path="/contact" element={<ContactPage/>}/>
+        <Route path="/catalog" element={<CatalogPage/>}/>
+        {/* если ничего не совпало*/}
+        <Route path="*" element={<HomePage/>}/>
+      </Routes>
+      <Footer/>
+      </>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,24 +2,15 @@ import React from 'react';
 import {BrowserRouter} from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import Location from './components/Location';
-import ContactHeader from './components/ContactHeader';
-import ShadowSlider from './components/ShadowSlider'
-import Footer from './components/Footer'
-import PreferencesContainer from './components/PreferencesContainer';
-import AboutUsSection from './components/AboutUsSection'
+// перенесено в pages homePage
+// в app находится header footer и весь роутинг 
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ContactHeader/>
-      <ShadowSlider/>
-      <PreferencesContainer/>
-      <AboutUsSection/>
-      <Location/>
-      <Footer/>
+      <App/>
     </BrowserRouter>
   </React.StrictMode>
 );
