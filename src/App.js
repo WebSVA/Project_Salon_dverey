54eb7e5bf7f4ba8e53622b0cdeec5f1a80c0bcec
Vikvillka <bechkovskayavic@gmail.com>
2024-11-22 22:19:56 +0300

    begin routing

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {Routes, Route, Link} from 'react-router-dom';
+
+import {ContactPage} from './pages/ContactPage';
+import {CatalogPage} from './pages/CatalogPage';
+import {AboutPage} from './pages/AboutPage';
+
+
+function App(){
+    return(
+        <header>
+            
+        </header>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import Location from './components/Location';
@@ -7,16 +8,18 @@ import ShadowSlider from './components/ShadowSlider'
 import Footer from './components/Footer'
 import PreferencesContainer from './components/PreferencesContainer';
 import AboutUsSection from './components/AboutUsSection'
-
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ContactHeader/>
-    <ShadowSlider/>
-    <PreferencesContainer/>
-    <AboutUsSection/>
-    <Location/>
-    <Footer/>
+    <BrowserRouter>
+      <ContactHeader/>
+      <ShadowSlider/>
+      <PreferencesContainer/>
+      <AboutUsSection/>
+      <Location/>
+      <Footer/>
+    </BrowserRouter>
   </React.StrictMode>
 );
