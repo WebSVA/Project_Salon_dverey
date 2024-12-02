51944648113199b98b4bd3b5c9e45043e1d19dcd
bluebox228 <gritskevitchstefania@gmail.com>
2024-12-02 22:40:51 +0300

    fixed burger and mail

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -2,64 +2,56 @@ import React from 'react';
 import '../styles/parallax.css';
 import Button from './Button';
 import Email from '../Email';
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
+
 const Parallax = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const modalOpen = ()=>{
-      setIsModalOpen(true);
-  }
-  const modalClose = ()=>{
+
+  const modalOpen = () => {
+    setIsModalOpen(true);
+  };
+
+  const modalClose = () => {
     setIsModalOpen(false);
-  }
+  };
 
   useEffect(() => {
     if (isModalOpen) {
-        document.body.style.overflow = 'hidden'; 
+      document.body.style.overflow = 'hidden'; 
     } else {
-        document.body.style.overflow = 'auto'; 
+      document.body.style.overflow = 'auto'; 
     }
 
     return () => {
-        document.body.style.overflow = 'auto';
+      document.body.style.overflow = 'auto';
     };
-}, [isModalOpen]);
+  }, [isModalOpen]);
 
-    return (  
-    <div class="parallax">
-  <div className="contact-section">
-      
-      <div className="contact-section2">
-      <p className="standout-heading">Остались вопросы?</p>
-      <p className="contact-info-text">Свяжитесь с нами по номеру телефона или оставьте заявку и мы свяжемся с Вами.</p>
-    </div>
+  return (  
+    <div className="parallax">
+      <div className="contact-section">
+        <div className="contact-section2">
+          <p className="standout-heading">Остались вопросы?</p>
+          <p className="contact-info-text">Свяжитесь с нами по номеру телефона или оставьте заявку и мы свяжемся с Вами.</p>
+        </div>
         <p className="contact-info-heading">
           <span>+</span>
           <span className="contact-number-text-style">8 029 928-92-89</span>
         </p>
-        {/* Button Component is detected here. We've generated code using HTML. See other options in "Component library" dropdown in Settings */}
-       
         <Button text="Оставить заявку" onClick={modalOpen} className="call-to-action-btn"/>
-    
-
       </div>
-       {/* Модальное окно */}
-       {isModalOpen && (
-                <div className="modal-overlay">
-                    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                        <Email />
-                        {/* <button onClick={modalClose}>x</button> */}
-                        <button  onClick={modalClose} className="close-modal-btn">x</button>
-    
-                        <div className="orang-strip"></div>
-                    </div>
-                    
-                </div>
-                
-            )}
+      {/* Модальное окно */}
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <Email modalClose={modalClose} />
+            <button onClick={modalClose} className="close-modal-btn">x</button>
+            <div className="orang-strip"></div>
+          </div>
+        </div>
+      )}
     </div>
-   
-  
-     );
+  );
 };
 
 export default Parallax;
