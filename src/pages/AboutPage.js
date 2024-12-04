c8b42d8d2308b83f14e1f97fac22ea1b84b19511
Vikvillka <bechkovskayavic@gmail.com>
2024-12-04 19:08:06 +0300

    test router

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,33 +3,11 @@ import Breadcrumbs from "../components/Breadcrumbs";
 import '../styles/app.css';
 
 function AboutPage() {
-    const [showScroll, setShowScroll] = useState(false);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            setShowScroll(window.scrollY > 300); // Показываем кнопку, если прокручено более 300px
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    const scrollToTop = () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' }); // Плавная прокрутка вверх
-    };
     
     return (
         <>
             <Breadcrumbs title="О нас" />
 
-            {showScroll && (
-                <button className="scroll-to-top" onClick={scrollToTop}>
-                    Вверх
-                </button>
-            )}
         </>
     );
 }
