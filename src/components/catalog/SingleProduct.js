dc9a9a06f14e15e645fba8700f0757768345b9c2
Vikvillka <bechkovskayavic@gmail.com>
2024-12-07 21:54:09 +0300

    style single product page

--- src/components/catalog/SingleProduct.js
+++ src/components/catalog/SingleProduct.js
@@ -1,12 +1,18 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import Button from '../Button';
 import data from "../../data/data.json";
 import '../../styles/catalog/singleProduct.css';
+import measureIcon from '../../assets/measure.png';
+import degreeIcon from '../../assets/degree.png';
+import toolsIcon from '../../assets/tools.png';
+import truckIcon from '../../assets/delivery-truck.png';
 
 function SingleProduct() {
     const { id } = useParams();
     const [product, setProduct] = useState(null);
     const [selectedImage, setSelectedImage] = useState(null);
+    const [selectedColor, setSelectedColor] = useState('');
 
     useEffect(() => {
         const foundProduct = data.find(item => item.id === parseInt(id));
@@ -14,11 +20,22 @@ function SingleProduct() {
             setProduct(foundProduct);
             const firstImage = Object.values(foundProduct.color)[0]; // Первая картинка из цветов
             setSelectedImage(Array.isArray(firstImage) ? firstImage[0] : firstImage); // Если массив, берем первый элемент
+
+            const defaultColor = Object.keys(foundProduct.color)[0]; // Берем первый цвет
+            setSelectedColor(defaultColor || '');
         }
     }, [id]);
 
     const handleImageClick = (imagePath) => {
         setSelectedImage(imagePath);
+
+         // Устанавливаем выбранный цвет на основе картинки
+         const color = Object.keys(product.color).find(colorName => 
+            Array.isArray(product.color[colorName]) 
+                ? product.color[colorName].includes(imagePath) 
+                : product.color[colorName] === imagePath
+        );
+        setSelectedColor(color || '');
     };
 
     return (
@@ -28,11 +45,13 @@ function SingleProduct() {
                     <div className="product-information-container">
                         <div className="product-img-container">
                             {/* Большая картинка */}
-                            <img 
-                                className="select-img-product" 
-                                src={selectedImage} 
-                                alt="Selected product" 
-                            />
+                            <div className="select-img-product-container">
+                                <img 
+                                    className="select-img-product" 
+                                    src={selectedImage} 
+                                    alt="Selected product" 
+                                />
+                            </div>
                             {/* Маленькие картинки */}
                             <div className="other-img-container">
                                 {Object.entries(product.color).map(([colorName, imagePath]) =>
@@ -49,29 +68,30 @@ function SingleProduct() {
                                         ))
                                     ) : (
                                         // Если одиночная картинка
-                                        <img
-                                            key={colorName}
-                                            src={imagePath}
-                                            alt={colorName}
-                                            className="small-img"
-                                            onClick={() => handleImageClick(imagePath)}
-                                        />
+                                        <div className="circle-container" key={colorName} onClick={() => handleImageClick(imagePath)}>
+                                            <img
+                                                src={imagePath}
+                                                alt={colorName}
+                                                className="small-img"
+                                            />
+                                        </div>
                                     )
                                 )}
                             </div>
                         </div>
                         <div className="single-product-info-container">
                             <h1>{product.name}</h1>
-                            {product.name && <p className="info-description"><span>Название:</span> {product.name}</p>}
-                            {product.type && <p className="info-description"><span>Тип:</span> {product.type}</p>}
-                            {product.material && <p className="info-description"><span>Материал:</span> {product.material}</p>}
-                            {product.glass && <p className="info-description"><span>Стекло:</span> {product.glass}</p>}
-                            {product.fill && <p className="info-description"><span>Наполнитель:</span> {product.fill}</p>}
-                            {product.thickness && <p className="info-description"><span>Толщина:</span> {product.thickness}</p>}
-                            {product.fabricator && <p className="info-description"><span>Производитель:</span> {product.fabricator}</p>}
-                            {product.eye && <p className="info-description"><span>Глазок:</span> {product.eye}</p>}
-                            {product.high_lock && <p className="info-description"><span>Верхний замок:</span> {product.high_lock}</p>}
-                            {product.low_lock && <p className="info-description"><span>Нижний замок:</span> {product.low_lock}</p>}
+                                {product.type && <div className="info-item"><p className="info-description"><span>Тип:</span> {product.type}</p></div>}
+                                {selectedColor && <div className="info-item"><p className="info-description"><span>Цвет:</span> {selectedColor}</p></div>} {/* Строка для выбранного цвета */}
+                                {product.material && <div className="info-item"><p className="info-description"><span>Материал:</span> {product.material}</p></div>}
+                                {product.glass && <div className="info-item"><p className="info-description"><span>Стекло:</span> {product.glass}</p></div>}
+                                {product.fill && <div className="info-item"><p className="info-description"><span>Наполнитель:</span> {product.fill}</p></div>}
+                                {product.thickness && <div className="info-item"><p className="info-description"><span>Толщина:</span> {product.thickness}</p></div>}
+                                {product.fabricator && <div className="info-item"><p className="info-description"><span>Производитель:</span> {product.fabricator}</p></div>}
+                                {product.eye && <div className="info-item"><p className="info-description"><span>Глазок:</span> {product.eye}</p></div>}
+                                {product.high_lock && <div className="info-item"><p className="info-description"><span>Верхний замок:</span> {product.high_lock}</p></div>}
+                                {product.low_lock && <div className="info-item"><p className="info-description"><span>Нижний замок:</span> {product.low_lock}</p></div>}
+
                             <div className="size-info-container">
                                 <h3>Доступные размеры:</h3>
                                 <div className="size-info">
@@ -82,12 +102,71 @@ function SingleProduct() {
                                     </ul>
                                 </div>
                             </div>
+                            <div className="text-description-to-button">
+                                <p>Наличие товара в магазине или любые другие вопросы уточняйте по номеру телефона или оставьте запрос  </p>
+                            </div>
+                            <div className="button-container">
+                                <Button className="link-to-form-detail-button" text="Связаться"></Button>
+                            </div>
                         </div>
                     </div>
                 </div>
             ) : (
                 <div>Продукт не найден</div>
             )}
+
+            <div className="services-container">
+                <div className="services-info-container">
+                    <div className="single-services-info">
+                        <div className="services-img-cotainer">
+                            <img  src={measureIcon} className="services-img" alt="Замеры"></img>
+                        </div>
+                        <div className="services-text-container">
+                            <h3>Замеры:</h3>
+                            <p>Замер бесплатный в пределах МКАД, при заказе от 3-х дверей с фурнитурой. Залог за услугу - 1500 руб.
+                                Услуга выезд менеджера замерщика с образцами покрытия стоит 2000 руб. по Москве в пределах МКАД.
+                                Доплата за выезд за МКАД + 50 руб./км.
+                                Замерщик приезжает в течении 1-2 дней после оформления заявки на замер.</p>
+                        </div>
+                    </div>
+                    <div className="single-services-info">
+                        <div className="services-img-cotainer">
+                            <img  src={measureIcon} className="services-img" alt="Замеры"></img>
+                        </div>
+                        <div className="services-text-container">
+                            <h3>Замеры:</h3>
+                            <p>Замер бесплатный в пределах МКАД, при заказе от 3-х дверей с фурнитурой. Залог за услугу - 1500 руб.
+                                Услуга выезд менеджера замерщика с образцами покрытия стоит 2000 руб. по Москве в пределах МКАД.
+                                Доплата за выезд за МКАД + 50 руб./км.
+                                Замерщик приезжает в течении 1-2 дней после оформления заявки на замер.</p>
+                        </div>
+                    </div>
+                    <div className="single-services-info">
+                        <div className="services-img-cotainer">
+                            <img  src={measureIcon} className="services-img" alt="Замеры"></img>
+                        </div>
+                        <div className="services-text-container">
+                            <h3>Замеры:</h3>
+                            <p>Замер бесплатный в пределах МКАД, при заказе от 3-х дверей с фурнитурой. Залог за услугу - 1500 руб.
+                                Услуга выезд менеджера замерщика с образцами покрытия стоит 2000 руб. по Москве в пределах МКАД.
+                                Доплата за выезд за МКАД + 50 руб./км.
+                                Замерщик приезжает в течении 1-2 дней после оформления заявки на замер.</p>
+                        </div>
+                    </div>
+                    <div className="single-services-info">
+                        <div className="services-img-cotainer">
+                            <img  src={measureIcon} className="services-img" alt="Замеры"></img>
+                        </div>
+                        <div className="services-text-container">
+                            <h3>Замеры:</h3>
+                            <p>Замер бесплатный в пределах МКАД, при заказе от 3-х дверей с фурнитурой. Залог за услугу - 1500 руб.
+                                Услуга выезд менеджера замерщика с образцами покрытия стоит 2000 руб. по Москве в пределах МКАД.
+                                Доплата за выезд за МКАД + 50 руб./км.
+                                Замерщик приезжает в течении 1-2 дней после оформления заявки на замер.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
