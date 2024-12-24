6698e483ecf761dd78d602289fc776ccbcf49ddd
bluebox228 <gritskevitchstefania@gmail.com>
2024-12-24 22:58:43 +0300

    adaptiv on filters

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,10 +1,12 @@
 import Breadcrumbs from "../components/Breadcrumbs";
 import ContactMap from "../components/ContactMap";
+import ContactForm from "../components/Contacts/ContactForm";
 import "../styles/contact.css"
 function ContactPage(){
     return(
         <>
         <Breadcrumbs title="Контакты" />
+<ContactForm></ContactForm>
         <div className="conmap"><ContactMap/></div>
         </>
     );
