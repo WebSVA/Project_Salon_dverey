6f8312baec9f908abbcb8898ae0abbad16ce0f27
bluebox228 <gritskevitchstefania@gmail.com>
2024-12-25 01:07:11 +0300

    some Form updates for ALESYA potomu chto ona ne moz'et work bez moego pushagit add .git add .

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,12 +1,24 @@
 import Breadcrumbs from "../components/Breadcrumbs";
 import ContactMap from "../components/ContactMap";
-import ContactForm from "../components/Contacts/ContactForm";
-import "../styles/contact.css"
+//import ContactForm from "../components/Contacts/ContactForm";
+import Email from "../Email";
+import "../styles/contact/contact.css"
+import "../styles/contact/contactForm.css"
 function ContactPage(){
     return(
         <>
         <Breadcrumbs title="Контакты" />
-<ContactForm></ContactForm>
+            <div className="contact-container-all">
+                <div className="contact-container">
+                     {/* <ContactForm></ContactForm> */}
+                     <div className="email-contact-container">
+                     <Email></Email>
+                     </div>
+                   
+                </div>
+           
+            </div>
+         
         <div className="conmap"><ContactMap/></div>
         </>
     );
