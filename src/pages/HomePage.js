0c96e88484b0b23652f1c2f44f4e2be0b1b98821
ale <alexoli2537@gmail.com>
2024-12-25 00:55:37 +0300

    meta-tags

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,11 +4,22 @@ import PreferencesContainer from '../components/PreferencesContainer';
 import AboutUsSection from '../components/AboutUsSection'
 import Parallax from '../components/Parallax';
 import UpCatalogSection from '../components/CatalogSection';
-
+import { Helmet } from "react-helmet";
 
 function HomePage(){
     return(
         <>
+          <Helmet>
+            
+            <title>Салон Дверей Сморгоньь – Качественные двери по доступным ценам.</title>
+            <meta name="description" content="Широкий ассортимент продукции, включая межкомнатные и входные двери, выполненные из высококачественных материалов" />
+            <meta name="keywords" content="двери, двери Сморгонь, входные двери, межкомнатные двери, купить двери, установка дверей, Сморгонь" />
+            <meta name="author" content="Салон дверей" />
+            <meta property="og:title" content="Качественные двери по доступным ценам – Салон дверей Сморгонь" />
+            <meta property="og:description" content="Широкий выбор дверей для дома и офиса. Профессиональная установка и доставка." />   
+            <meta property="og:type" content="website" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          </Helmet>
         <ShadowSlider/>
         <PreferencesContainer/>
         <UpCatalogSection/>
